cc302cfbf85d1eff5331be9d810c75a07be3f0dc
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-05-30 01:06:11 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import Ruler from "./Ruler.jsx";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Ruler />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>International Humanitarian Law</li>
+      <li>Public/Private International Law</li>
+      <li>Alternative Dispute Resolution</li>
+      <li>Intellectual Property Law</li>
+      <li>Cyber Laws</li>
+      <li>Corporate & Commercial Laws</li>
+      <li>Environmental Law</li>
+    </ul>
+  );
+}
+
+export default List;
